a28b1646628885fd9de5d0457e6f3d21efb70fec
Harsh Kumar <harsh.mca19.du@gmail.com>
2023-03-05 16:00:20 +0530

    adds required dependencies

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,20 @@
-import express from "express";
+import express, { Response, Request } from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import helmet from "helmet";
+
 const app = express();
 const port = 3001;
+app.use(cors());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(helmet());
+
+app.get("/", (req:Request, res:Response) => {
   res.send("Hello World!");
 });
 
